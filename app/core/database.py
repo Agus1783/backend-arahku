abc5e8f99783d9ac5b29f283da97f2e1e9fa91d1
unknown <17agus03@gmail.com>
2026-05-26 12:23:01 +0700

    persiapan deploy backend

--- app/core/database.py
+++ app/core/database.py
@@ -1,19 +1,26 @@
-# import os
+import os
 
-from app.core.config import settings
+# from app.core.config import settings
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 _ = load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"mysql+pymysql://{settings.DB_USER}"
-    f":{settings.DB_PASSWORD}"
-    f"@{settings.DB_HOST}"
-    f":{settings.DB_PORT}"
-    f"/{settings.DB_NAME}"
-)
+if DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace(
+        "mysql://",
+        "mysql+pymysql://",
+        1,
+    )
+# DATABASE_URL = (
+#     f"mysql+pymysql://{settings.DB_USER}"
+#     f":{settings.DB_PASSWORD}"
+#     f"@{settings.DB_HOST}"
+#     f":{settings.DB_PORT}"
+#     f"/{settings.DB_NAME}"
+# )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=True)
 
